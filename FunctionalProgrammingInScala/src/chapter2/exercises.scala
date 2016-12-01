2ad737dcb1f3b345e261b570b7fcbd3e3119811b
WilliamMyers <willy@wdmyers.co.uk>
2016-12-01 15:26:01 +0000

    next one

--- FunctionalProgrammingInScala/src/chapter2/exercises.scala
+++ FunctionalProgrammingInScala/src/chapter2/exercises.scala
@@ -5,14 +5,19 @@ import scala.annotation.tailrec
 object exercises {
 
   def main(args: Array[String]): Unit = {
-    println(fib(5))
-    println(
-        isSorted(Array[Int](1,2,3,4,5,6), ((x:Int,y:Int) => x <=y))
-        )
+    println(fib(5)) //returns 3
+    println(isSorted(Array[Int](1,2,3,4,5,6), ((x:Int,y:Int) => x <=y))) // returns true
+    println(isSorted(Array[Int](1,2,3,4,7,5,6), ((x:Int,y:Int) => x <=y))) // returns false
+    
+    val c = curry((a:Int, b:Int) => a+b)
+    val add4 = c(4) // our new function that adds 4 to whatever is passed to it
+    println(add4(8)) // should be 12
+    println(add4(4)) // should be 8
+    println(add4(2)) // should be 6
   }
 
   /**
-   * A recursive function to get the nth Fibonacci number.
+   * 2.1 A recursive function to get the nth Fibonacci number.
    * First 2 are 0 and 1 and the nth is always the sum of the previous 2 - the sequence begins 0,1,1,2,3,5
    */
     def fib(n: Int) = {
@@ -28,15 +33,28 @@ object exercises {
     }
     
     /**
-     * A recursive function that checks an array is in sorted order
+     * 2.2 A recursive function that checks an array is in sorted order
      */
     def isSorted[A](as : Array[A], f: (A,A) => Boolean): Boolean = {
       def loop(n: Int): Boolean = {
-        if(n >= as.length -1) true
-        else if (f(as(n), as(n+1))) loop(n+1)
-        else false
+        if(n >= as.length -1) true //got to the end
+        else if (f(as(n), as(n+1))) loop(n+1) //still sorted so keep going
+        else false //f() has returned false, it's all over
       }
       loop(0)
     }
     
+    /**
+     * 2.3 Currying function that converts a function of 2 args into a function of one arg that partially applies the first
+     */
+    def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+      a => b => f(a, b)
+    }
+      
+    /**
+     * 2.4 Reverse the transformation of curry
+     */
+    def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+      (a,b) => f(a)(b)
+    }
 }
